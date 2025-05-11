7e133f4ad91ad1408faee7b3a17dff7957405b83
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-05-11 21:48:50 +0600

    file structure updated

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -2,17 +2,20 @@ import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({ required: true })
   name: string;
 
   @Prop({ unique: true })
   email: string;
 
-  @Prop()
+  @Prop({ required: false })
   password: string;
 
   @Prop({ required: false })
   image: string;
+
+  @Prop({ required: true })
+  provider: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
