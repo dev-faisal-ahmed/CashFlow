3b33607a964a7364f680e74f733b67416ed38afe
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-05-16 01:47:13 +0600

    client project initiated

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { PropsWithChildren } from "react";
+
+const font = Inter({ subsets: ["latin"], weight: ["400", "600", "700"] });
+
+export const metadata: Metadata = {
+  title: "Cash Flow",
+  description: "Manages your cashflow",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
