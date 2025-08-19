da7c29bd0b05099e56d6709789afcf090a0508d3
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-08-20 01:04:03 +0600

    fixed transaction module

--- src/server/modules/transaction/transaction.repository.ts
+++ src/server/modules/transaction/transaction.repository.ts
@@ -2,19 +2,24 @@ import { ClientSession } from "mongoose";
 import { ETransactionNature, ETransactionType, IInitialTransaction, ITransferTransaction } from "./transaction.interface";
 import { InitialTransaction, TransferTransaction } from "./transaction.schema";
 
-export class TransactionRepository {
-  async createInitialTransaction(dto: CreateInitialTransactionDto, session: ClientSession) {
-    return InitialTransaction.create([{ ...dto, type: ETransactionType.initial, nature: ETransactionNature.income }], { session });
-  }
+// Types
 
-  async createTransferTransaction(dto: CreateTransferTransactionDto) {
-    return TransferTransaction.create(dto);
-  }
-}
+type CreateInitialTransaction = {
+  dto: Pick<IInitialTransaction, "ownerId" | "amount" | "walletId" | "description">;
+  session: ClientSession;
+};
 
-type CreateInitialTransactionDto = Pick<IInitialTransaction, "ownerId" | "amount" | "walletId" | "description">;
-
-type CreateTransferTransactionDto = Pick<
+type CreateTransferTransaction = Pick<
   ITransferTransaction,
   "ownerId" | "amount" | "sourceWalletId" | "destinationWalletId" | "description"
 >;
+
+export class TransactionRepository {
+  async createInitialTransaction({ dto, session }: CreateInitialTransaction) {
+    return InitialTransaction.create([{ ...dto, type: ETransactionType.initial, nature: ETransactionNature.income }], { session });
+  }
+
+  async createTransferTransaction(dto: CreateTransferTransaction) {
+    return TransferTransaction.create(dto);
+  }
+}
